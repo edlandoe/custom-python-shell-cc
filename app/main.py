68b4025d86d66c9a8e7d55bd04a7daa17a6608fb
Edlandoe <Landoeliacin@gmail.com>
2024-05-25 18:58:06 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,18 +3,16 @@ import sys
 
 def main():
     commands = {}
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    # print("Logs from your program will appear here!")
 
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    # Wait for user input
-    command = input()
-    if command not in commands:
-       sys.stdout.write(f"{command}: command not found\n") 
-       sys.stdout.flush()
+        # Wait for user input
+        command = input()
+        if command not in commands:
+            sys.stdout.write(f"{command}: command not found\n") 
+            sys.stdout.flush()
     
 if __name__ == "__main__":
     main()
