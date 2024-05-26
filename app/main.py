265f04335d6c8c5a0c8d292dc1fde6f81f3fb8fd
Edlandoe <Landoeliacin@gmail.com>
2024-05-25 20:38:55 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,28 +1,36 @@
 import sys
 
 def exit(args: str):
-    if args[0] == "0":
-        sys.exit(0)
-    else:
-        sys.stdout.write(f"Need proper arguments\n")
+    if args:
+        sys.exit(int(args[0]))
     
 def echo(args: str):
     sys.stdout.write(f"{" ".join(args)}\n")
+    sys.stdout.flush()
     
-def main():
-    commands = {
+def type(args: str):
+    if args[0] in commands:
+        sys.stdout.write(f"{args[0]} is a shell builtin\n")
+        sys.stdout.flush()
+    else:
+        sys.stdout.write(f"{args[0]} not found\n")
+        sys.stdout.flush()
+        
+commands = {
         "exit": exit,
-        "echo": echo
+        "echo": echo,
+        "type": type
     }
 
+def main():
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
         initial_input = input()  # Wait for user input 
         command = initial_input.split(" ")[0]
-        args = [""]
-        if len(command) > 1:
+        args = []
+        if len(initial_input.split(" ")) > 1:
             args = initial_input.split(" ")[1:]
         
         if command not in commands:
