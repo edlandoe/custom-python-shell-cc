98b8aca904365623412d9c0df1fd8ae887155a71
Edlandoe <Landoeliacin@gmail.com>
2024-05-27 09:54:53 -0400

    test

--- app/main.py
+++ app/main.py
@@ -1,43 +1,24 @@
+from shell import Shell
 import sys
 
-def exit(args: str):
-    if args:
-        sys.exit(int(args[0]))
-    
-def echo(args: str):
-    sys.stdout.write(f"{" ".join(args)}\n")
-    sys.stdout.flush()
-    
-def type(args: str):
-    if args[0] in commands:
-        sys.stdout.write(f"{args[0]} is a shell builtin\n")
-        sys.stdout.flush()
-    else:
-        sys.stdout.write(f"{args[0]} not found\n")
-        sys.stdout.flush()
-        
-commands = {
-        "exit": exit,
-        "echo": echo,
-        "type": type
-    }
-
 def main():
+    shell = Shell()   
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
-        initial_input = input()  # Wait for user input 
-        command = initial_input.split(" ")[0]
+        initial_input = input().split(" ")  # Wait for user input 
+        command = initial_input[0]
         args = []
-        if len(initial_input.split(" ")) > 1:
-            args = initial_input.split(" ")[1:]
         
-        if command not in commands:
+        if len(initial_input) > 1:
+            args = initial_input[1:]
+        
+        if command not in shell.get_commands():
             sys.stdout.write(f"{command}: command not found\n") 
             sys.stdout.flush()
         else:
-            commands[command](args)
+           shell.run_command(command, args)
 
 
 if __name__ == "__main__":
---
+++ app/shell.py
@@ -0,0 +1,50 @@
+from typing import Callable
+import sys, os
+
+class Shell:
+    def __init__(self):
+        self.commands = {
+        "exit": self.exit,
+        "echo": self.echo,
+        "type": self.type
+    }
+    
+    # Helper Functions        
+    def _get_command_path(self, command: str) -> str | None: 
+        env_var = os.getenv("PATH")
+        directories = env_var.split(":")
+        
+        for directory in directories:
+            path = os.path.join(directory, command)
+            if os.path.exists(path):
+                return path
+            
+        return None
+        
+    # Class Functions
+    def get_commands(self) -> dict[str, Callable[[list[str]], None]]:
+        return self.commands
+    
+    def run_command(self, command: str, args: list[str]) -> Callable[[list[str]], None]:
+        return self.commands[command](args)
+      
+    def exit(self, args: list[str]) -> None:
+        if args:
+            sys.exit(int(args[0]))
+        
+    def echo(self, args: list[str]) -> None:
+        sys.stdout.write(f"{" ".join(args)}\n")
+        sys.stdout.flush()
+       
+    def type(self, args: list[str]) -> None:
+        if args[0] in self.commands:
+            sys.stdout.write(f"{args[0]} is a shell builtin\n")
+            sys.stdout.flush()
+        elif path := self._get_command_path(args[0]):
+            sys.stdout.write(f"{args[0]} is {path}\n")
+            sys.stdout.flush()
+        else:
+            sys.stdout.write(f"{args[0]} not found\n")
+            sys.stdout.flush()
+            
+    
